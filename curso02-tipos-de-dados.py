9ee2636248cd4643565e0659f485ce3e70f81f0d
IcaroNascimento <104856582+icaronascimento-git@users.noreply.github.com>
2023-08-06 11:30:57 -0300

    Adicionando curso 02 e RAEDME

---
+++ curso02-tipos-de-dados.py
@@ -0,0 +1,13 @@
+print(10 + 8 + 36)
+print(5 * 6)
+print("Python")
+
+age, name = (26, 'Ícaro Nascimento')
+print(f'Meu nome é {name} e tenho {age} anos(s).')
+
+age, name = (27, 'Ícaro Gabriel')
+print(f'Meu nome é {name} e tenho {age} anos')
+
+limite_saque_diario = 1000 # Padrão snake_case
+
+STATES_BRAZILIAN = ('SP', 'MG', 'AC', 'MT') # Variável toda em maiúscula para indicar que se trata de constante
