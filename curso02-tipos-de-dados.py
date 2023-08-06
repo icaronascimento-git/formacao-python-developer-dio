264922c77cea692930b2b6d9f5cbfb8d69133af4
IcaroNascimento <104856582+icaronascimento-git@users.noreply.github.com>
2023-08-06 18:33:49 -0300

    Finalização aula02

--- curso02-tipos-de-dados.py
+++ curso02-tipos-de-dados.py
@@ -6,8 +6,26 @@ age, name = (26, 'Ícaro Nascimento')
 print(f'Meu nome é {name} e tenho {age} anos(s).')
 
 age, name = (27, 'Ícaro Gabriel')
-print(f'Meu nome é {name} e tenho {age} anos')
+print(f'Meu nome é {name} e tenho {age} anos') # O f no ínicio da string indica que vou usar variáveis nessa string
 
 limite_saque_diario = 1000 # Padrão snake_case
 
 STATES_BRAZILIAN = ('SP', 'MG', 'AC', 'MT') # Variável toda em maiúscula para indicar que se trata de constante
+
+print(int(1.9)) # O int coverte o valor quebrado 1.9 em valor inteiro
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+print(51 / 3) # Aqui será realizada a divisão e o resultado mostrará os quebrados
+print(51 // 3) # Aqui será realizada a divisão e as barras "//" duplas indicam que será retornado o valor inteiro da divisão
+
+#nome1 = input("Informe o seu nome: ") # o input faz com que o texto seja exibido na tela para o usuário inserir um dado, esse dado inserido será atribuido a variável nome
+
+nome = "Gabriel"
+sobrenome = "Silva"
+
+print(nome, sobrenome) # Aqui será exibido na tela o nome e o sobrenome, conforme declarado na variável.
+print(nome, sobrenome, end="...\n") # Nesse exemplo estou pedindo para ser adicionado três pontos ao final e o \n fara uma quebra de linha.
+print(nome, sobrenome, sep="#") # Nesse exemplo será adicionado o nome e o sobrenome e o sep irá adicionar um separador.
+
