6aabc13674f5e43d082065715be0def7c4659e45
IcaroNascimento <104856582+icaronascimento-git@users.noreply.github.com>
2023-08-09 22:17:21 -0300

    commit curso 07 tuplas

---
+++ 02 - Estrutura de Dados/curso07 - Tuplas.py
@@ -0,0 +1,11 @@
+# EXEMPLOS DE TUPLAS
+
+verduras = ("chu-chu", "abrobinha", "muranga", "pepino",)
+# è uma boa prática na declaração de tuplas entre parenteses, inserir uma vírgula no final do ultimo elemento. Se não inserir o Python pode confundir como uma precedência de operadores.
+
+letras = tuple("Developer") # Aqui utiloizamos o CONSTRUTOR TUPLA e passamos a string.
+
+# Diferente da lista que é declarada entre colchetes []. As tuplas são declaradas entre parênteses ().
+
+
+
